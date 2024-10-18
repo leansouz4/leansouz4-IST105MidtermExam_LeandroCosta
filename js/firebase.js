099f2ac5b8b6e219b7f7f010cc53b8513290dbf2
Leandro Souza <Leandrosouzag2@gmail.com>
2024-10-17 21:08:23 -0700

    Firebase config

--- js/firebase.js
+++ js/firebase.js
@@ -9,14 +9,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCYgZ3iYEgzhBySZpChKCFSyAaWmErgVHE",
-    authDomain: "socialnetworkproject-4898b.firebaseapp.com",
-    projectId: "socialnetworkproject-4898b",
-    storageBucket: "socialnetworkproject-4898b.appspot.com",
-    messagingSenderId: "801361369319",
-    appId: "1:801361369319:web:3bf59b38205e46b79caa42",
-    measurementId: "G-DBDQW69PJ7"
-  };
+  apiKey: "AIzaSyAl0FqiZkI2HXevSLagtLCFLZD9y4vwlpQ",
+  authDomain: "ist105midtermexam-leandrocosta.firebaseapp.com",
+  projectId: "ist105midtermexam-leandrocosta",
+  storageBucket: "ist105midtermexam-leandrocosta.appspot.com",
+  messagingSenderId: "854385990503",
+  appId: "1:854385990503:web:a01afa814b7f05921cedc2"
+};
   
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
