113584ce750c06d2c9b361a6c327e970eb5936e7
Leandro Souza <Leandrosouzag2@gmail.com>
2024-10-17 20:36:14 -0700

    First Commit

---
+++ js/firebase.js
@@ -0,0 +1,25 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCYgZ3iYEgzhBySZpChKCFSyAaWmErgVHE",
+    authDomain: "socialnetworkproject-4898b.firebaseapp.com",
+    projectId: "socialnetworkproject-4898b",
+    storageBucket: "socialnetworkproject-4898b.appspot.com",
+    messagingSenderId: "801361369319",
+    appId: "1:801361369319:web:3bf59b38205e46b79caa42",
+    measurementId: "G-DBDQW69PJ7"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  
+  
---
+++ js/js/home.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function () {
+
+    //Listen for auth state changes
+    firebase.auth().onAuthStateChanged(onAuthStateChanged);
+
+    //Implement SignOut Function
+
+
+});
