5523e96f3752f4a20f383ea32ab5c3e1c7cc5973
Leandro Souza <Leandrosouzag2@gmail.com>
2024-11-02 21:48:20 -0700

    teste final

--- IST105MidtermExam_LeandroCostaa/js/firebase.js
+++ IST105MidtermExam_LeandroCostaa/js/firebase.js
@@ -1,20 +1,21 @@
-// Importa as funções necessárias do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
-import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Configuração do Firebase
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAl0FqiZkI2HXevSLagtLCFLZD9y4vwlpQ",
   authDomain: "ist105midtermexam-leandrocosta.firebaseapp.com",
   projectId: "ist105midtermexam-leandrocosta",
-  storageBucket: "ist105midtermexam-leandrocosta.appspot.com",
+  storageBucket: "ist105midtermexam-leandrocosta.firebasestorage.app",
   messagingSenderId: "854385990503",
   appId: "1:854385990503:web:a01afa814b7f05921cedc2"
 };
 
-// Inicializa o Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-// Exporta o auth para ser usado em outros arquivos
-export { auth, signInWithPhoneNumber, RecaptchaVerifier };
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
