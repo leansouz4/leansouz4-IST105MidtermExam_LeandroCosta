f795734e089c57700019b22ce65cc1ea89dbbd89
Leandro Souza <Leandrosouzag2@gmail.com>
2024-11-02 21:58:15 -0700

    login

--- IST105MidtermExam_LeandroCostaa/js/firebase.js
+++ IST105MidtermExam_LeandroCostaa/js/firebase.js
@@ -5,17 +5,17 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAl0FqiZkI2HXevSLagtLCFLZD9y4vwlpQ",
   authDomain: "ist105midtermexam-leandrocosta.firebaseapp.com",
   projectId: "ist105midtermexam-leandrocosta",
-  storageBucket: "ist105midtermexam-leandrocosta.firebasestorage.app",
+  storageBucket: "ist105midtermexam-leandrocosta.appspot.com",
   messagingSenderId: "854385990503",
   appId: "1:854385990503:web:a01afa814b7f05921cedc2"
 };
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
