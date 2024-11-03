7835d15210c463bb84b7cde470d6533752ab2ee7
Leandro Souza <Leandrosouzag2@gmail.com>
2024-11-02 23:12:12 -0700

    js

--- IST105MidtermExam_LeandroCostaa/js/firebase.js
+++ IST105MidtermExam_LeandroCostaa/js/firebase.js
@@ -1,14 +1,11 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-/*
-  CCTB Website Development
-  IST105
-  Oct 2024
-  Description: This is a simple login website where students are asked to 
-  implement Social Network Login with Firebase
-*/
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyALgiY-zLXvBmv-b9rflTAoLJ4ZQdscfv0",
   authDomain: "newwproject-abfec.firebaseapp.com",
@@ -18,9 +15,7 @@ const firebaseConfig = {
   appId: "1:178437666218:web:98e3d883ac965b1d0267d3",
   measurementId: "G-Z7NNF5QMB6"
 };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  
-  
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
